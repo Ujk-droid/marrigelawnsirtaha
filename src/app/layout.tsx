ce6caf18bddbf11fb8409fb26185af55fb9fb82a
Ujk-droid <03312436713aa@gmail.com>
2025-06-08 01:48:53 +0500

    Update message1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className='dark'>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="flex h-screen bg-background">
